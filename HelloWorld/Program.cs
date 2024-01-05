2af3764ba16916130ce3835f33c3938ec2da4964
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-05 16:29:15 -0300

    Tipos de texto

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,30 @@ class Program
 {
     static void Main()
     {
-       bool ativo = false;
-       bool ativo2 = true;
+       /*
+       Variáveis do tipo TEXTO
+       char
+       string
+       */
+
+        char letra = 'a';
+        char numero = '1';
+        char caracter = '@';
+        char espaco = ' ';
+
+        string texto = "Este curso é muito bom!";
+
+        char primeiraLetraDoTexto = texto[0];
+        bool existe1 = texto.Contains("curso");
+        bool existe2 = texto.Equals("Este curso é muito bom!");
+
+        Console.WriteLine(existe1);
+
+        string meuNome = "      Welisson Dev      ";
+        string nomeSemEspaco = meuNome.Trim();
+
+        bool comecaComALetraW = nomeSemEspaco.StartsWith("W");
+        bool terminaComALetraV = nomeSemEspaco.EndsWith("v");
+        string textoAposReplace = nomeSemEspaco.Replace('e', '7');
     }
 }
