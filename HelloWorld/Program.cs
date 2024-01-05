330569e4eceb8d552cc71cad05c7cdf3e4b178b8
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-05 16:04:58 -0300

    Tipo booleano

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,25 +4,7 @@ class Program
 {
     static void Main()
     {
-        /* 
-        Inteiros:
-
-        int numero1 = 7;
-        long numero2 = 7;
-        uint numero3 = 7;
-        sbyte numero4 = 127;
-        byte numero5 = 255;
-        */
-
-        /* Flutuantes - Decimais */
-
-        double numero1 = 3.14;
-        float numero2 = 0.14f;
-        decimal numero3 = 3.14m;
-
-        int numero4 = 1_000;
-
-
-        Console.WriteLine(numero3);
+       bool ativo = false;
+       bool ativo2 = true;
     }
 }
