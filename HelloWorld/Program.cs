d1463128a0d4668a1ac5b3c285c0c08f84a60af5
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-08 12:33:39 -0300

    Array

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,30 @@ class Program
 {
     static void Main()
     {
-        int? idade = null;
+        /* Início da aula
 
-        int minhaIdade = idade.Value;
-        bool informouIdade = idade.HasValue;
+        int[] inteiros = new int[10];
+
+        inteiros[0] = 1;
+        inteiros[1] = 10;
+        inteiros[2] = 7;
+        
+
+        int[] inteiros = [1, 10, 7];
+
+        Console.WriteLine(inteiros[0]);
+        Console.WriteLine(inteiros[1]);
+        Console.WriteLine(inteiros[2]);
+
+        */
+
+        int[,] inteiros = new int[10, 10];
+        inteiros[0,0] = 1;
+        inteiros[0,3] = 2;
+
+        Console.WriteLine(inteiros[0, 0]);
+        Console.WriteLine(inteiros[0, 1]);
+        Console.WriteLine(inteiros[0, 2]);
+        Console.WriteLine(inteiros[0, 3]);
     }
 }
