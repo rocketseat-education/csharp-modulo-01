fdd9cb108336c5b2d9b54fdde9cfdc26f7c20210
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-08 14:55:26 -0300

    HashSets

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,24 +4,13 @@ class Program
 {
     static void Main()
     {
-       /* Exemplo 1:
-       Dictionary<int, string> dicionario = new Dictionary<int, string>();
-       dicionario.Add(1, "Welisson");
-       dicionario.Add(58, "Edilaine");
-       dicionario.Add(7, "Willian");
+       HashSet<int> set = new HashSet<int>();
 
-       string value = dicionario[2];
-       */
+        set.Add(1);
+        set.Add(2);
 
-        Dictionary<string, int> dicionario = new Dictionary<string, int>();
-        dicionario.Add("Welisson", 1);
-        dicionario.Add("Edilaine", 0);
-        dicionario.Add("Willian", 7);
+        set.Add(1);
 
-        int value = dicionario["Welisson"];
-        bool existe = dicionario.ContainsKey("Welisson");
-
-        Console.WriteLine(value);
-        Console.WriteLine(existe);
+        Console.WriteLine(set.Count);
     }
 }
