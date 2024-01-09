31d0defc76db667b3ff859c6c92e3f1cda52dcd8
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-09 14:10:48 -0300

    Looping - For

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,13 +5,11 @@ class Program
     static void Main()
     {
         var lista = new List<string> { "Welisson", "Willian", "Edilaine" };
-        var dicionario = new Dictionary<string, string>();
 
-        dicionario.Add("Nome1", "Welisson");
-        dicionario.Add("Nome2", "Willian");
-        dicionario.Add("Nome3", "Edilaine");
+        for (int i = 0; i < 10; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
 
-        foreach (var item in lista) 
-            Console.WriteLine(item);
     }
 }
