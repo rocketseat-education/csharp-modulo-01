8065fa409fd21c82d8e01eab43b78c9f67484227
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-09 14:16:32 -0300

    Condicionais Else e if - parte I

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,14 +1,67 @@
 ﻿namespace HelloWorld;
 
 class Program
-{
+{   enum Cores
+    {
+        Vermelho,
+        Azul,
+        Amarelo
+    }
     static void Main()
-    { 
-        var carro1 = new Carro("Carro 1");
+    {
+        int numero = 0;
+        double saldo = 100.50;
+        bool ativo = true;
+        string autor = "Welisson";
+        List<int> lista = new List<int> { 1, 7 };
+        Cores cor = Cores.Azul;
 
-        var carro2 = new Carro("Carro 2");
+        /* Operadores:
+            > Maior
+            < Menor
+            >= Maior ou igual
+            <= Menor ou igual
+            == Igualdade
+            != Diferente
+        */
 
-        carro1.NomeDoModelo();
-        carro2.NomeDoModelo();
+
+        /* Condições com o numero */
+        if (numero > 0)
+        {
+            Console.WriteLine("Este número é POSITIVO");
+        }
+        else if (numero == 0)
+        {
+            Console.WriteLine("Este número é NEUTRO");
+        }
+        else
+        {
+            Console.WriteLine("Este número é NEGATIVO");
+        }
+
+        /* Condições com o booleano */
+        if (ativo)
+        {
+            Console.WriteLine("ENTROUUU");
+        }
+
+        /* Condições com a string */
+        if (autor.Equals("Welisson"))
+        {
+            Console.WriteLine("ENTROUUU");
+        }
+
+        /* Condições com a lista */
+        if (lista.Count > 0) 
+        {
+            Console.WriteLine("A lista tem pelo menos um valor!");
+        }
+
+        /* Condições com o enum */
+        if (cor == Cores.Azul)
+        {
+            Console.WriteLine("ENTROUUU");
+        }
     }
 }
