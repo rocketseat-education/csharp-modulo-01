8717e9616b2076edfbfb2afb4621469f612f708b
Lara Azevedo <lara.azevedo@outlook.com>
2024-01-09 13:21:29 -0300

    Switch

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,12 +2,102 @@
 
 class Program
 {
+
+    enum Cores
+    {
+        Azul,
+        Vermelho,
+        Amarelo,
+        Verde,
+        Roxo
+    }
+
+
     static void Main()
     {
+        string texto = "Welisson";
+        Cores cor = Cores.Azul;
         int numero = 7;
+        
+        /* Usando o exemplo da cor */
+        switch (cor)
+        {
+            case Cores.Azul:
+                {
+                    Console.WriteLine("Azul");
+                }
+                break;
+            case Cores.Vermelho:
+                {
+                    Console.WriteLine("Vermelho");
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("Outra cor");
+                }
+                break;
+        }
 
-        string autor = numero == 7 ? "welisson" : "willian";
+        /* Usando exemplo da string */
+        switch (texto)
+        {
+            case "Welisson":
+                {
+                    Console.WriteLine("Welisson");
+                }
+                break;
+            case "Willian":
+                {
+                    Console.WriteLine("Willian");
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("Outro nome");
+                }
+                break;
+        }
 
-        Console.WriteLine(autor);
+        /* Usando exemplo dos números */
+        switch (numero)
+        {
+            case > 7:
+                {
+                    Console.WriteLine("O número é maior que 7");
+                }
+                break;
+            case 1:
+                {
+                    Console.WriteLine("O número é 1");
+                }
+                break;
+            case 0:
+                {
+                    Console.WriteLine("O número é 0");
+                }
+                break;
+        }
+
+        /* Switch ternário com números */
+        string resultado = numero switch
+        {
+            7 => "Welisson",
+            1 => "Willian",
+            3 => "Edilaine",
+            _ => "Nome desconhecido"
+        };
+
+        /* Switch ternário com enums */
+        string resultado1 = cor switch
+        {
+            Cores.Azul => "Welisson",
+            Cores.Amarelo => "Willian",
+            Cores.Verde => "Edilaine",
+            _ => "Nome desconhecido"
+        };
+
+        Console.WriteLine(resultado);
+        Console.WriteLine(resultado1);
     }
 }
